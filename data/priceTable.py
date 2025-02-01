49fad48a7c5fab262da6cc1de850f8d2b4c17d50
andres-delgado <andresdelgado0395@gmail.com>
2025-02-01 13:00:44 -0800

    read price and craft files

---
+++ data/priceTable.py
@@ -0,0 +1,21 @@
+
+class PriceTable:
+  def __init__(self):
+
+    # TODO: change data structure
+    self.alchemy = [0, 0]
+    self.augmentation = [0, 0]
+    self.regal = [0, 0]
+    self.vaal = [0, 0]
+    self.exalted = [0, 0]
+
+    self.ire = [0, 0]
+    self.guilt = [0, 0]
+    self.greed = [0, 0]
+    self.paranoia = [0, 0]
+    self.envy = [0, 0]
+    self.disgust = [0, 0]
+    self.despair = [0, 0]
+    self.fear = [0, 0]
+    self.suffering = [0, 0]
+    self.isolation = [0, 0]
